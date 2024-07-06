83ba2f6df2232b060a2a3c6e5b46ac8320ee65f7
Rainax1 <prikshitraina268@gmail.com>
2024-07-06 22:45:49 +0530

    exercise 1, incomplete yet.

--- ex1.dart
+++ ex1.dart
@@ -1,5 +1,6 @@
 /*
-Create a program that asks the user to enter their name and their age. Print out a message that tells how many years they have to be 100 years old.
+Create a program that asks the user to enter their name and their age.
+Print out a message that tells how many years they have to be 100 years old.
 
 */
 import "dart:io";
